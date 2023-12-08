1afaa05b50a8762e771b5e45e927d5ce84a373ae
Augustine <austynomilano77@gmail.com>
2023-12-08 05:37:26 -0800

    base model initialised

---
+++ models/base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+The base model of all the methods and attributes of the AirBnB Application
+"""
+import models
+import uuid
+from datetime import datetime
+
+
+class BseModel():
+    '''All public attributes and methds are housed here'''
+    
