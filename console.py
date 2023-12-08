9bb0bc4c9e5970bdeb0b812efef569741a7bada9
Augustine <austynomilano77@gmail.com>
2023-12-08 04:29:52 -0800

    exit functions integrated

--- console.py
+++ console.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 import cmd
+import sys
 
 '''the Console which handles the main program'''
 
 class HBNBCommand(cmd.Cmd):
     '''the command line interpreter'''
     prompt = 'Airbnb cmd: '
-    intro = "Simple command processor example."
+    intro = "command processor for the Airbnb Enjoy...."
+
+    def do_exit(self, line):
+        '''Exit the program'''
+        print('Exited!')
+        sys.exit()
+
+    def do_quit(self, line):
+        '''Exit the program'''
+        self.do_exit(line)
+
+    def do_EOF(self, line):
+        '''Exit the program'''
+        print('Exited!')
+        sys.exit()
 
 
 if __name__ == '__main__':
