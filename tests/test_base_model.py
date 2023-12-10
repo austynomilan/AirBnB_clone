4392c3ce5fa07f6f1a7ab6b4d74400d9fad5d9cc
Augustine <austynomilano77@gmail.com>
2023-12-10 14:44:41 -0800

    test for instantiation added

---
+++ tests/test_base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.base_model import BaseModel
+
+class TestBaseModel_instantiation(unittest.TestCase):
+    '''Testing for the instantiation of the basemodel'''
+    def test_instantiation(self):
+        '''Test instantiations'''
+        model = BaseModel()
+        self.assertIsInstance(model, BaseModel)
+        self.assertTrue(hasattr(model, 'id'))
+        self.assertTrue(hasattr(model, 'created_at'))
+        self.assertTrue(hasattr(model, 'updated_at'))
+
+    def test_instantiation_with_kwargs(self):
+        """Test instantiation with keyword arguments"""
+	kwargs = {
+            'id': '123',
+            'created_at': '2023-01-01T00:00:00.000000',
+            'updated_at': '2023-01-01T00:00:00.000000'
+        }
+        model = BaseModel(**kwargs)
+	for key, value in kwargs.items():
+            self.assertEqual(getattr(model, key), value if key not in 
+            ['created_at', 'updated_at']
+            else datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+
