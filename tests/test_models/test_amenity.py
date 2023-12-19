e00fa0efb80e57ff6807a439712b1f45f1aa3084
Tommytinny <tommyteenboi@gmail.com>
2023-12-19 01:11:56 +0100

    added new test case

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Unittest for amenity.py
+"""
+import unittest
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """
+    Test cases for Amenity class
+    """
+    def test_amenity_attr(self):
+        """
+        Test if attribute is created
+        """
+        attr = Amenity()
+        self.assertEqual(attr.name, "")
+
+if __name__ == '__main__':
+    unittest.main()
