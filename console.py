31cf63bd00730055031c66339c242d3e9ca31ef5
Tommytinny <tommyteenboi@gmail.com>
2023-12-22 18:24:43 +0100

    fix bugs

--- console.py
+++ console.py
@@ -137,133 +137,97 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, arg):
         """Updates an instance based on the class name and id"""
         args = self.parse_arg(arg)
-
+        class_name = args[0]
+        classes = [key.split('.')[0] for key in storage.all().keys()]
         if len(args) < 1:
             print("** class name missing **")
-            return
-
-        class_name = args[0]
-
-        classes = [key.split('.')[0] for key in storage.all().keys()]
-        if class_name not in classes:
+        elif class_name not in classes:
             print("** class doesn't exist **")
-            return
-
-        if len(args) < 2:
+        elif len(args) < 2:
             print("** instance id missing **")
-            return
-
-        instance_id = args[1]
-        key = f"{class_name}.{instance_id}"
-        instances = storage.all()
-        instance = instances.get(key)
-
-        if not instance:
-            print("** no instance found **")
-            return
-
-        if len(args) < 3:
-            print("** attribute name missing **")
-            return
-
-        attribute_name = args[2]
-
-        if len(args) < 4:
-            print("** value missing **")
-            return
-
-        attribute_value = args[3]
-
-        if len(args) > 4:
-            print("** only one attribute can be updated at a time **")
-            return
-
-        try:
-            casted_value = str(attribute_value)
-            setattr(instance, attribute_name, casted_value)
-            instance.save()
-            print(f"Attribute {attribute_name} of {class_name} instance "
-                  f"{instance_id} updated to {casted_value}.")
-        except ValueError:
-            print("** invalid attribute value **")
+        else:
+            instance_id = args[1]
+            key = f"{class_name}.{instance_id}"
+            instances = storage.all()
+            instance = instances.get(key)
+            if not instance:
+                print("** no instance found **")
+            elif len(args) < 3:
+                print("** attribute name missing **")
+            elif len(args) < 4:
+                print("** value missing **")
+            elif len(args) > 4:
+                print("** only one attribute can be updated at a time **")
+            else:
+                attribute_name = args[2]
+                attribute_value = args[3]
+                casted_value = str(attribute_value)
+                setattr(instance, attribute_name, casted_value)
+                instance.save()
 
     def default(self, arg):
         """ Handle arbitrary input """
-
-        try:
-            class_name, method_call = arg.split(".", 1)
-
-            """ handles show() command """
-            if method_call.startswith("show(") and method_call.endswith(")"):
-                instance_id = method_call[6:-2]
-                key = f"{class_name}.{instance_id}"
-                instances = storage.all()
-                instance = instances.get(key)
-                classes = [key.split('.')[0] for key in storage.all().keys()]
-                if class_name in classes:
-                    if instance:
-                        print(instance)
-                    else:
-                        print("** no instance found **")
+        class_name, method_call = arg.split(".", 1)
+        """ handles show() command """
+        if method_call.startswith("show(") and method_call.endswith(")"):
+            instance_id = method_call[6:-2]
+            key = f"{class_name}.{instance_id}"
+            instances = storage.all()
+            instance = instances.get(key)
+            classes = [key.split('.')[0] for key in storage.all().keys()]
+            if class_name in classes:
+                if instance:
+                    print(instance)
                 else:
-                    print("** class doesn't exist **")
-            elif method_call.startswith("all(") and method_call.endswith(")"):
-                """ handles all() command """
+                    print("** no instance found **")
+            else:
+                print("** class doesn't exist **")
+        elif method_call.startswith("all(") and method_call.endswith(")"):
+            """ handles all() command """
 
-                classes = [key.split('.')[0] for key in storage.all().keys()]
-                if class_name in classes:
-                    print([str(instance)
-                           for key, instance in storage.all().items()
-                           if key.startswith(f"{class_name}.")
-                           ])
+            classes = [key.split('.')[0] for key in storage.all().keys()]
+            if class_name in classes:
+                print([str(instance)
+                        for key, instance in storage.all().items()
+                        if key.startswith(f"{class_name}.")
+                        ])
+            else:
+                print("** class doesn't exist **")
+        elif method_call.startswith("destroy("):
+            """ handles destroy() command """
+
+            instance_id = method_call[9:-2]
+            key = f"{class_name}.{instance_id}"
+            instances = storage.all()
+            instance = instances.get(key)
+            classes = [key.split('.')[0] for key in storage.all().keys()]
+
+            if class_name in classes:
+                if key in instances:
+                    del instances[key]
+                    storage.save()
                 else:
-                    print("** class doesn't exist **")
-            elif method_call.startswith("destroy("):
-                """ handles destroy() command """
-
-                instance_id = method_call[9:-2]
-                key = f"{class_name}.{instance_id}"
-                instances = storage.all()
-                instance = instances.get(key)
-                classes = [key.split('.')[0] for key in storage.all().keys()]
-
-                if class_name in classes:
-                    if key in instances:
-                        del instances[key]
-                        storage.save()
-                    else:
-                        print("** no instance found **")
-                else:
-                    print("** class doesn't exist **")
-            elif method_call.startswith("update("):
-                """ handles update() command """
-
-                getArg = method_call[7:-1]
-                getParam = [arg.strip(' "\'') for arg in getArg.split(',', 2)]
-                """getParam = [arg for arg in getArg.split(',', 1)]"""
-                classes = [key.split('.')[0] for key in storage.all().keys()]
-                if "{" in getArg and "}" in getArg:
-                    print("Dictionary is present")
-                if class_name not in classes:
-                    print("** class doesn't exist **")
-                    return
-
-                if len(getParam) < 1:
-                    print("** instance id is missing **")
-                    return
-
+                    print("** no instance found **")
+            else:
+                print("** class doesn't exist **")
+        elif method_call.startswith("update("):
+            """ handles update() command """
+            getArg = method_call[7:-1]
+            getParam = [arg.strip(' "\'') for arg in getArg.split(',', 2)]
+            classes = [key.split('.')[0] for key in storage.all().keys()]
+            if class_name not in classes:
+                print("** class doesn't exist **")
+            elif len(getParam) < 1:
+                print("** instance id is missing **")
+            else:
                 instance_id = getParam[0]
                 key = f"{class_name}.{instance_id}"
                 instances = storage.all()
                 instance = instances.get(key)
-
                 if not instance:
                     print("** no instance found **")
-                    return
-
-                if "{" in getArg and "}" in getArg:
+                elif "{" in getArg and "}" in getArg:
                     comma_index = getArg.index(',')
-
                     instance_id = getArg[:comma_index].strip(' "\'')
                     dicts = getArg[comma_index + 1:].strip()
                     my_dict = literal_eval(dicts)
@@ -271,52 +235,32 @@ class HBNBCommand(cmd.Cmd):
                         casted_value = str(value)
                         setattr(instance, key, casted_value)
                         instance.save()
-                        """print(f"Attribute {attr_name} of "
-                                f"{class_name} instance "
-                                 f"{instance_id} updated "
-                                 f"to {casted_value}.")"""
-                    return
-
-                if len(getParam) < 2:
+                elif len(getParam) < 2:
                     print("** attribute name missing **")
-                    return
-
-                attr_name = getParam[1]
-
-                if len(getParam) < 3:
+                elif len(getParam) < 3:
                     print("** value missing **")
-                    return
-
-                attr_value = getParam[2]
-
-                if len(getParam) > 3:
+                elif len(getParam) > 3:
                     print("** only one attribute can be updated at a time **")
-                    return
-                try:
+                else:
+                    attr_name = getParam[1]
+                    attr_value = getParam[2]
                     casted_value = str(attr_value)
                     setattr(instance, attr_name, casted_value)
                     instance.save()
-                    """print(f"Attribute {attr_name} of {class_name} instance "
-                            f"{instance_id} updated to {casted_value}.")"""
-                except ValueError:
-                    print("** invalid attribute value **")
-            elif method_call.startswith("count("):
-                """ handles count() command """
-                classes = [key.split('.')[0] for key in storage.all().keys()]
-                instances = storage.all()
-                if class_name in classes:
-                    instance_count = 0
-                    for className in storage.all():
-                        if className.split('.')[0] == class_name:
-                            instance_count += 1
-                    print("{}".format(instance_count))
-                else:
-                    print("** class doesn't exist **")
-                    return
+        elif method_call.startswith("count("):
+            """ handles count() command """
+            classes = [key.split('.')[0] for key in storage.all().keys()]
+            instances = storage.all()
+            if class_name in classes:
+                instance_count = 0
+                for className in storage.all():
+                    if className.split('.')[0] == class_name:
+                        instance_count += 1
+                print("{}".format(instance_count))
             else:
-                print("*** Unknown command: {}".format(arg))
-        except Exception as e:
-            print(f"An error occured: {e}")
+                print("** class doesn't exist **")
+        else:
+            print("*** Unknown command: {}".format(arg))
 
 
 if __name__ == '__main__':
---
+++ test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+import inspect
+import io
+import sys
+import cmd
+import console
+
+"""
+ Create console
+"""
+console_obj = "HBNBCommand"
+for name, obj in inspect.getmembers(console):
+    if inspect.isclass(obj) and issubclass(obj, cmd.Cmd):
+        console_obj = obj
+
+my_console = console_obj(stdout=io.StringIO(), stdin=io.StringIO())
+my_console.use_rawinput = False
+
+"""
+ Exec command
+"""
+def exec_command(my_console, the_command, last_lines = 1):
+    my_console.stdout = io.StringIO()
+    real_stdout = sys.stdout
+    sys.stdout = my_console.stdout
+    my_console.preloop()
+    the_command = my_console.precmd(the_command)
+    my_console.onecmd(the_command)
+    sys.stdout = real_stdout
+    lines = my_console.stdout.getvalue().split("\n")
+    return "\n".join(lines[(-1*(last_lines+1)):-1])
+
+"""
+ Tests
+"""
+model_class = "BaseModel"
+model_id = "Nop"
+attribute_name = "attribute_name"
+attribute_value = "string_value"
+
+result = exec_command(my_console, "{}.update(\"{}\", \"{}\", \"{}\")".format(model_class, model_id, attribute_name, attribute_value))
+is_error = False
+if result is None or result == "":
+    pass  
+elif result == "** no instance found **":
+    is_error = True
+
+if not is_error:
+    result = exec_command(my_console, "{}.update({}, \"{}\", \"{}\")".format(model_class, model_id, attribute_name, attribute_value))
+    if result is None or result == "":
+        pass  
+    elif result == "** no instance found **":
+        is_error = True
+
+if not is_error:
+    result = exec_command(my_console, "{}.update(\"{}.{}\", \"{}\", \"{}\")".format(model_class, model_class, model_id, attribute_name, attribute_value))
+    if result is None or result == "":
+        pass  
+    elif result == "** no instance found **":
+        is_error = True
+
+if not is_error:
+    result = exec_command(my_console, "{}.update({}.{}, \"{}\", \"{}\")".format(model_class, model_class, model_id, attribute_name, attribute_value))
+    if result is None or result == "":
+        pass  
+    elif result == "** no instance found *":
+        is_error = True
+
+if not is_error:
+    print("FAIL: not found")
+    
+print("OK", end="")
